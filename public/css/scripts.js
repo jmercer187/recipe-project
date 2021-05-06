b8f0d58ce767c20bb87dbf82b171ee5be3b23c8f
jmercer187 <jake.mercer@gmail.com>
2021-05-06 17:51:55 -0400

    started flexboxin

---
+++ public/css/scripts.js
@@ -0,0 +1,11 @@
+const addIngredientBtn = document.getElementById('add-ingredient-btn"');
+const ingredient = document.getElementById('ingredient');
+const ingredientsContainer = document.getElementById('ingredients-container');
+
+// add ingredients functions
+const addIngredientPill = () => {
+
+}
+
+// event listeners
+addIngredientBtn.addEventListener('click', addIngredientPill)
